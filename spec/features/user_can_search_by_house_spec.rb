6c89d187171e940f72ddc267561810283983df9e
“Neeru <neeram85@gmail.com>
2020-09-01 09:18:52 -0600

    Write feature test

---
+++ spec/features/user_can_search_by_house_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "User can search for Order of the Phoenix members by House" do
+  scenario "User submits valid house name" do
+
+    visit '/'
+
+    select 'Gryffindor', from: :house
+
+    click_on 'Search For Members'
+
+    expect(current_path).to eq(search_path)
+
+    #expect(page).to have_content("# Results")
+
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".house")
+      expect(page).to have_css(".patronus")
+    end
+  end
+end
