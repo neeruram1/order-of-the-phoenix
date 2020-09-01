6db42233581eafc6c8e4f31d3477c862bb40db7e
“Neeru <neeram85@gmail.com>
2020-09-01 10:33:07 -0600

    Refactored PotterService

--- app/POROS/search_results.rb
+++ app/POROS/search_results.rb
@@ -1,7 +1,7 @@
 class SearchResults
 
   def self.members_by_house(house)
-    json = PotterService.members_by_house(house)
+    json = PotterService.new.members_by_house(house)
     @members = json.map do |member_data|
       Member.new(member_data)
     end
--- app/services/potter_service.rb
+++ app/services/potter_service.rb
@@ -1,11 +1,19 @@
 class PotterService
 
-  def self.members_by_house(house)
-    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |f|
+  def members_by_house(house)
+    to_json("characters?orderOfThePhoenix=true&house=#{house}")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "https://www.potterapi.com/v1/") do |f|
       f.params[:key] = ENV['POTTER_API_KEY']
     end
+  end
 
-    response = conn.get("characters?orderOfThePhoenix=true&house=#{house}")
+  def to_json(url)
+    response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
