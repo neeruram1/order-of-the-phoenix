e205370eeebf9886922b976d213390ad677464cc
“Neeru <neeram85@gmail.com>
2020-09-01 10:00:58 -0600

    Create index action for search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
 class SearchController < ApplicationController
   def index
+    house = params[:house]
 
+    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |f|
+      f.params[:key] = ENV['POTTER_API_KEY']
+    end
+
+    response = conn.get("characters?orderOfThePhoenix=true&house=#{house}")
+    binding.pry
   end
 end
