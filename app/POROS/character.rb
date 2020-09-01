e45972af131dcf10f1f07f4f4bc3e56a4262995c
“Neeru <neeram85@gmail.com>
2020-09-01 10:11:06 -0600

    Initialize and attributes added to Character class

--- app/POROS/character.rb
+++ app/POROS/character.rb
@@ -1,2 +1,13 @@
 class Character
+  attr_reader :name,
+              :role,
+              :house,
+              :patronus
+
+  def initialize(attributes)
+    @name       = attributes[:name]
+    @role       = attributes[:role]
+    @party      = attributes[:house]
+    @district   = attributes[:patronus]
+  end
 end
